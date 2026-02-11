dde03450ab69ce41374cfe4ed78ef3e90810e0a8
unknown <matheusmag.dev@gmail.com>
2026-02-11 13:05:46 -0300

    Integração com API para consultar clientes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,62 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-clientes');
+
+    //Criando um objeto da classe HttpClient
+    private http = inject(HttpClient);
+
+    //Atributo para armazenar os dados da consulta de clientes
+    clientes = signal<any[]>([]);
+
+    //Atributo para guardar o endereço da API
+    private apiUrl = 'http://localhost:8081/api/v1/clientes';
+
+    //Criando a estrutura do formulário de cadastro de clientes
+    formCadastro = new FormGroup({ //formulário
+      nome : new FormControl(''), //campo 'nome'
+      email : new FormControl(''), //campo 'email'
+      telefone : new FormControl('') //campo 'telefone'
+    });
+
+    //Criando a estrutura do formulário de consulta
+    formConsulta = new FormGroup({
+      nome : new FormControl('') //campo 'nome' para consulta
+    });
+
+    //Função para realizar o cadastro do cliente
+    cadastrar() {
+
+      //Fazendo uma requisição HTTP POST para a API
+      this.http.post(this.apiUrl, this.formCadastro.value, { responseType: 'text' })
+        .subscribe((mensagem) => { //Aguardando o retorno da requisição
+            alert(mensagem); //Exibir a mensagem para o usuário
+            this.formCadastro.reset(); //Limpar o formulário
+        }); 
+    }
+
+
+    //Função para realizar a consulta de clientes
+    consultar() {
+
+      //Fazendo uma requisiçao HTTP GET para a API, passando o nome como parâmetro
+      this.http.get(this.apiUrl + '/' + this.formConsulta.value.nome)
+        .subscribe((clientes) => {
+          this.clientes.set(clientes as any[]); //Atualizar o sinal com os dados dos clientes
+        }); //Aguardando o retorno da requisição e exibindo os clientes encontrados
+
+    }
+  
 }
